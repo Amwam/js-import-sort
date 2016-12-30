0cca8ca3e1c65de1934425d88ad1eb175b476857
Amit Shah <amit@amwam.me>
2016-12-30 15:25:45 +0000

    Fix bad import of lib/omit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Runner = require('jscodeshift/dist/Runner');
 const resolve = require('path').resolve;
-const omit  = require('lib/omit');
+const omit  = require('./lib/omit');
 
 module.exports = args => {
     const options = Object.assign({ quote: 'single' }, omit(args, ['_', 'transform', 'path']));
